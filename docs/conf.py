2f1fbfb64d33aac7a6c1b99fd7c51ce1e8739af5
Calvin <calvinchau@outlook.de>
2021-05-17 20:18:27 +0200

    updated conf

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import mock
+import sys
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'pysmps', 'pulp']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
