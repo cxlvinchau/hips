726c6debbbac11035615f407bd327a35e48d358f
severin <ga58fok@mytum.de>
2021-05-24 10:53:30 +0200

    Added Diving heuristics

--- src/hips/heuristics/__init__.py
+++ src/hips/heuristics/__init__.py
@@ -18,5 +18,7 @@ def skip_when_clp_solver(func):
 
 from hips.heuristics._heur_feasibility_pump import FeasibilityPump
 from hips.heuristics._heur_bounds import HeuristicBounds
+from hips.heuristics._heur_abstract_diving import AbstractDiving
+from hips.heuristics._heur_fractional_diving import FractionalDivingHeuristic
 
-__all__ = ["FeasibilityPump", "HeuristicBounds"]
+__all__ = ["FeasibilityPump", "HeuristicBounds", "AbstractDiving", "FractionalDivingHeuristic"]
