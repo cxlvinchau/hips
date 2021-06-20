7101f751882e4b956e3b1b0de4dab6ad427f3e2e
Calvin <calvinchau@outlook.de>
2021-06-20 09:54:51 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 import setuptools
 
-setup(name="hips-integer-programming",
+with open("requirements.txt") as file:
+      requirements = file.readlines()
+
+setup(name="hips-mip",
       version="0.3dev",
       author="Calvin Chau",
       author_email="calvin.chau@tum.de",
       package_dir={"": "src"},
       packages = setuptools.find_packages(where="hips"),
       python_requires=">=3.8",
+      install_requires=requirements
       )
