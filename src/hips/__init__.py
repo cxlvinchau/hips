9b6310529446d761de64d559d0ddaa2624f61e33
Severin <severin1997@gmx.de>
2021-07-04 14:24:26 +0200

    added BoundDirection to init

--- src/hips/__init__.py
+++ src/hips/__init__.py
@@ -1,5 +1,5 @@
 from hips.models import HIPSArray
-from hips.constants import ProblemSense, Comparator, VarTypes, VariableBound, NUMERICAL_TYPES, LPStatus, HeuristicStatus
+from hips.constants import ProblemSense, Comparator, VarTypes, VariableBound, NUMERICAL_TYPES, LPStatus, HeuristicStatus, BoundDirection
 from hips.problems import load_problem, load_problem_gurobi, load_problem_clp
 
 CLP_AVAILABLE = False
